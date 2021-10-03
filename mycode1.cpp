34ef208d52853163c1beb418472105c17afa9dbe
Samita Parajuli <samitap@uw.edu>
2021-10-03 12:58:45 -0700

    Added a new .cpp file

---
+++ mycode1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int foo(int  y)
+{
+    y--;
+    return y;
+}
+
+int main()
+{
+    int x = 9;
+    const int z = 10;
+    cout << foo(x) << '\n'
+        << foo(z);
+
+    return 0;
+}
